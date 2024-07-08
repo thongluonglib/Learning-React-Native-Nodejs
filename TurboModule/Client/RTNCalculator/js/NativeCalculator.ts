6c0a838bd1447e710bb702af2dd74acb45574bf6
lvthong312 <lvthong312@gmail.com>
2024-07-08 08:55:45 +0700

    init turbo module

---
+++ TurboModule/Client/RTNCalculator/js/NativeCalculator.ts
@@ -0,0 +1,8 @@
+import { TurboModule, TurboModuleRegistry } from "react-native";
+
+export interface Spec extends TurboModule {
+  add(a: number, b: number): Promise<number>;
+  sub(a: number, b: number): Promise<number>;
+}
+
+export default TurboModuleRegistry.get<Spec>("RTNCalculator") as Spec | null;
